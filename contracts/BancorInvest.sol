0b717f93f6b5adc2dab666bb9a64e96ad1a31194
Lecky Lao <leckylao@gmail.com>
2020-04-05 23:55:12 +1000

    added function approve and fund with test;

--- contracts/BancorInvest.sol
+++ contracts/BancorInvest.sol
@@ -5,10 +5,20 @@ import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
 contract BancorInvest is Initializable, GSNRecipient {
   address private _owner;
+  address public eth_token;
+  address public bnt_token;
+  address public converter;
+
+  event ETHApproval(address indexed _owner, address indexed _spender, uint256 _value);
+  event BNTApproval(address indexed _owner, address indexed _spender, uint256 _value);
+  event Fund(address indexed _owner, uint256 _value);
 
   function initialise() public initializer {
     GSNRecipient.initialize();
     _owner = _msgSender();
+    eth_token = address(0x62bd9D98d4E188e281D7B78e29334969bbE1053c);
+    bnt_token = address(0xD368b98d03855835E2923Dc000b3f9c2EBF1b27b);
+    converter = address(0xF5fe6280db283ba6975d72A3bD39bF57840433F7);
   }
 
   // accept all requests
@@ -22,7 +32,7 @@ contract BancorInvest is Initializable, GSNRecipient {
     uint256,
     bytes calldata,
     uint256
-    ) external view returns (uint256, bytes memory) {
+  ) external view returns (uint256, bytes memory) {
     return _approveRelayedCall();
   }
 
@@ -44,5 +54,19 @@ contract BancorInvest is Initializable, GSNRecipient {
 
   function getRecipientBalance() public view returns (uint) {
     return IRelayHub(getHubAddr()).balanceOf(address(this));
+  }
+
+  function approve(address _spender, uint256 _value) public
+  {
+    eth_token.call(abi.encodePacked("approve(address, uint256)", _spender, _value));
+    emit ETHApproval(msg.sender, _spender, _value);
+    bnt_token.call(abi.encodePacked("approve(address, uint256)", _spender, _value));
+    emit BNTApproval(msg.sender, _spender, _value);
+  }
+
+  function fund(uint256 _amount) public
+  {
+    converter.call(abi.encodePacked("fund(uint256)", _amount));
+    emit Fund(msg.sender, _amount);
   }
 }
--- test/bancor_invest.js
+++ test/bancor_invest.js
@@ -22,4 +22,16 @@ describe('BancorInvest', function () {
   it("should have proper owner", async () => {
     (await bancor_invest.owner()).should.equal(owner);
   });
+
+  it("should have emit Approval", async () => {
+    let approve = await bancor_invest.approve("0xF5fe6280db283ba6975d72A3bD39bF57840433F7", "1000000000000000000");
+    expectEvent(approve, 'ETHApproval', {_spender: "0xF5fe6280db283ba6975d72A3bD39bF57840433F7", _value: "1000000000000000000" });
+    expectEvent(approve, 'BNTApproval', {_spender: "0xF5fe6280db283ba6975d72A3bD39bF57840433F7", _value: "1000000000000000000" });
+
+  });
+
+  it('should be able to fund', async () => {
+    let fund = await bancor_invest.fund("1000000000000000000");
+    expectEvent(fund, 'Fund', {_value: "1000000000000000000" });
+  });
 });
